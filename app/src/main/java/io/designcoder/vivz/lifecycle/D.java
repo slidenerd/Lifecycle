219075b468b13ae2b11486fcddef52baacd98230
slidenerd <slidenerd@gmail.com>
2015-12-26 11:22:23 +0530

    Displays the Fragment Lifecycle in the logcat if you choose the menu from the main screen and select the appropriate option

---
+++ app/src/main/java/io/designcoder/vivz/lifecycle/D.java
@@ -0,0 +1,24 @@
+package io.designcoder.vivz.lifecycle;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class D extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_d);
+
+    }
+
+}
